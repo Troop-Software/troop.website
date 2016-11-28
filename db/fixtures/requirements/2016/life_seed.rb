52742f5fb5f8292592b120dfe875e12e54077afd
bob <rkoertge@salesforce.com>
2016-11-28 15:31:40 -0500

    Added Life  Requirements

---
+++ db/fixtures/requirements/2016/life_seed.rb
@@ -0,0 +1,20 @@
+Requirement.seed(sortOrder: 1,req_num: '1', revision: '2016', rank_id:7, req_category: :general,  description: 'Be active in your troop for at least six months as a Star Scout.')
+Requirement.seed(sortOrder: 2,req_num: '2', revision: '2016', rank_id:7, req_category: :general,  description: 'As a Star Scout, demonstrate Scout spirit by living the Scout Oath and Scout Law. Tell how you have done your duty to God and how you have lived the Scout Law in your everyday life.')
+Requirement.seed(sortOrder: 3,req_num: '3', revision: '2016', rank_id:7, req_category: :general,  description: 'Earn five more merit badges (so that you have 11 in all), including any three additional badges from the required list for Eagle. You may choose any of the 17 merit badges on the required list for Eagle to fulfill this requirement. See Eagle rank requirement #3 for this list.')
+Requirement.seed(sortOrder: 4,req_num: '4', revision: '2016', rank_id:7, req_category: :general,  description: 'While a Star Scout, participate in six hours of service through one or more service projects approved by your Scoutmaster. At least 3 hours of this service must be conservation related.')
+Requirement.seed(sortOrder: 5,req_num: '5', revision: '2016', rank_id:6, req_category: :general,  description: 'While a Star Scout, serve actively in your troop for four months in one or more of the following positions of responsibility (or carry out a Scoutmaster-approved leadership project to help the troop):
+Boy Scout troop. Patrol leader, assistant senior patrol leader, senior patrol leader, troop guide, Order of the Arrow troop representative, den chief, scribe, librarian, historian, quartermaster, bugler, junior assistant Scoutmaster, chaplain aide, instructor, webmaster, or outdoor ethics guide.4
+Varsity Scout team. Captain, cocaptain, program manager, squad leader, team secretary, Order of the Arrow team representative, librarian, historian, quartermaster, chaplain aide, instructor, den chief, webmaster, or outdoor ethics gide.
+Venturing crew/Sea Scout ship. President, vice president, secretary, treasurer, den chief, quartermaster, historian, guide, boatswain, boatswain’s mate, yeoman, purser, storekeeper, or webmaster.
+Lone Scout. Leadership responsibility in your school, religious organization, club, or elsewhere in your community.')
+Requirement.seed(sortOrder: 6,req_num: '6', revision: '2016', rank_id:7, req_category: :general,  description: "While a Star Scout, use the Teaching EDGE method to teach another Scout (preferably younger than you) the skills from ONE of the following choices, so that he is prepared to pass those requirements to his Scoutmaster's satisfaction.
+a. Tenderfoot - 4a and 4b (first aid)
+b. Second Class - 2b, 2c, and 2d (cooking/camping)
+c. Second Class - 3a and 3d(navigation)
+d. First Class - 3a, 3b, 3c, and 3d (tools)
+e. First Class - 4a and 4b (navigation)
+f. Second Class - 6a and 6b (first aid)
+g. First Class - 7a and 7b (first aid)
+Three requirements from one of the required Eagle merit badges, as approved by your Scoutmaster.")
+Requirement.seed(sortOrder: 7,req_num: '7', revision: '2016', rank_id:7, req_category: :general,  description: 'While a Star Scout, participate in a Scoutmaster conference')
+Requirement.seed(sortOrder: 8,req_num: '8', revision: '2016', rank_id:7, req_category: :general,  description: 'Successfully complete your board of review for the Life rank.')
