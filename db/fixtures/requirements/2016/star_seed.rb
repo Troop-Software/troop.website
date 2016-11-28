796c881f4ea95f0f9b22f5a414193f0c35eccecf
bob <rkoertge@salesforce.com>
2016-11-28 15:24:09 -0500

    Added Star Requirements

---
+++ db/fixtures/requirements/2016/star_seed.rb
@@ -0,0 +1,14 @@
+Requirement.seed(sortOrder: 1,req_num: '1', revision: '2016', rank_id:6, req_category: :general,  description: 'Be active in your troop for at least four months as a First Class Scout.')
+Requirement.seed(sortOrder: 2,req_num: '2', revision: '2016', rank_id:6, req_category: :general,  description: 'As a First Class Scout, demonstrate Scout spirit by living the Scout Oath and Scout Law. Tell how you have done your duty to God and how you have lived the Scout Oath and Scout Law in your everyday life')
+Requirement.seed(sortOrder: 3,req_num: '3', revision: '2016', rank_id:6, req_category: :general,  description: 'Earn six merit badges, including any four from the required list for Eagle. You may choose any of the 17 merit badges on the required list for Eagle to fulfill this requirement. See Eagle rank requirement 3 for this list.')
+Requirement.seed(sortOrder: 4,req_num: '4', revision: '2016', rank_id:6, req_category: :general,  description: 'While a First Class Scout, participate in six hours of service through one or more service projects approved by your Scoutmaster.')
+Requirement.seed(sortOrder: 5,req_num: '5', revision: '2016', rank_id:6, req_category: :general,  description: 'While a First Class Scout, serve actively in your troop for four months in one or more of the following positions of responsibility (or carry out a Scoutmaster-approved leadership project to help the troop):
+Boy Scout troop. Patrol leader, assistant senior patrol leader, senior patrol leader, troop guide, Order of the Arrow troop representative, den chief, scribe, librarian, historian, quartermaster, bugler, junior assistant Scoutmaster, chaplain aide, instructor, webmaster, or outdoor ethics guide.4
+Varsity Scout team. Captain, cocaptain, program manager, squad leader, team secretary, Order of the Arrow team representative, librarian, historian, quartermaster, chaplain aide, instructor, den chief, webmaster, or outdoor ethics gide.
+Venturing crew/Sea Scout ship. President, vice president, secretary, treasurer, den chief, quartermaster, historian, guide, boatswain, boatswain’s mate, yeoman, purser, storekeeper, or webmaster.
+Lone Scout. Leadership responsibility in your school, religious organization, club, or elsewhere in your community.')
+Requirement.seed(sortOrder: 6,req_num: '6', revision: '2016', rank_id:6, req_category: :general,  description: 'With your parent or guardian, complete the exercises in the pamphlet "How to Protect Your Children from Child Abuse: A Parents Guide" and earn the Cyber Chip Award for your grade.')
+Requirement.seed(sortOrder: 7,req_num: '6', revision: '2016', rank_id:6, req_category: :general,  description: 'While a First Class Scout, participate in a Scoutmaster conference')
+Requirement.seed(sortOrder: 8,req_num: '6', revision: '2016', rank_id:6, req_category: :general,  description: 'Successfully complete your board of review for the Star rank.')
+
+
