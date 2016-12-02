3ea5548e6425d4293e1070f33c9aaac422ee7b8e
bob <rkoertge@salesforce.com>
2016-12-02 00:54:22 -0500

    added none to position seed fixes #17 added unassigned to patrol seed fixes #16 fixed scout detail print view number_to_phone error fixes #20

--- db/fixtures/patrols_seed.rb
+++ db/fixtures/patrols_seed.rb
@@ -1,4 +1,4 @@
-patrol_names =['Blood Hounds', 'Screaming Eagles', 'Black Hawks', 'Morning', 'Wolverines']
+patrol_names =['unassigned','Blood Hounds', 'Screaming Eagles', 'Black Hawks', 'Morning', 'Wolverines']
 
 patrol_names.each do |patrol|
   Patrol.seed(name: patrol)
