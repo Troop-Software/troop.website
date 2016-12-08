a233f0a1f337683b683c8649c1df1ae9bc59cbdc
Bob Koertge <bob@bobkoertge.com>
2016-12-08 08:50:43 +0000

    add category to events, added categories to calendar

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,20 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :start, presence: true
+  validates :category, presence: true
+  
+  enum category: [:troop_meeting, :campout, :outing, :plc, :committee_meeting, 
+                    :service_project, :training, :other]
+  
+  
+  def self.search(search)
+    if search
+      #search by id
+      #where('category LIKE ?', "%#{search}%")
+      #seach exact match
+      where(category: Event.categories[search.parameterize.underscore.to_sym])
+    else
+      all
+    end
+  end
 end
