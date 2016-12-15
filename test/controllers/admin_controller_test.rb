4ca64088dd4ba558d078aeb1ce9a1ace5737a916
Bob Koertge <bob@bobkoertge.com>
2016-12-15 18:57:13 +0000

    fixed a test

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
 
-  test 'admin home is not accessible if not logged in' do
-    get admin_home_url
+  test 'admin user is not accessible if not logged in' do
+    get admin_users_path
     assert_redirected_to user_session_path
   end
 
