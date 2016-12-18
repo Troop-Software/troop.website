33724252ce7ebfa7be4b8a7ff32c63dc7f765c8d
bob <rkoertge@salesforce.com>
2016-12-17 22:00:35 -0500

    publc / private routing and controllers setup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,5 @@
 class StaticPagesController < ApplicationController
 
-  layout 'application', only: [:home]
-
   def home
     @articles = Article.paginate(page: params[:page], per_page: 8).order('updated_at DESC')
   end
