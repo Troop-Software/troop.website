c31cb2b1485b2e2d913bb77d742545d0ff508830
bob <rkoertge@salesforce.com>
2016-12-19 22:40:20 -0500

    resolved conflict

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,9 @@
 class PublicController < ApplicationController
 
+<<<<<<< HEAD
   def eagle_roll
     @eagle_scouts = Scout.where(rank_id: 8)
   end
+=======
+>>>>>>> 0493e8c... publc / private routing and controllers setup
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
+<<<<<<< HEAD
   
+=======
+
+  layout 'application', only: [:home]
+
+>>>>>>> 0493e8c... publc / private routing and controllers setup
   def home
     @articles = Article.search_title(params[:search]).paginate(page: params[:page], per_page: 8).order('updated_at DESC')
   end
