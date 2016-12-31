531ee04ae8b95f8cac787a008674da11b34189a5
bob <rkoertge@salesforce.com>
2016-12-31 18:18:40 -0500

    Generate Scout Details report for currently viewed scout, or all scouts if no scout is selected

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,15 +2,19 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_user_leader
 
-def scout_detail_report
-  @scouts = Scout.where(active: true)
-  @scouts = Scout.all if current_user.show_inactive_scouts
+  def scout_detail_report
+    if params[:id]
+      @scout = Scout.find(params[:id])
+    else
+      @scouts = Scout.where(active: true)
+      @scouts = Scout.all if current_user.show_inactive_scouts
+    end
 
-  respond_to do |format|
-    format.pdf {
-      send_data render_to_string, filename: 'Scout Detailed Report.pdf', type: 'application/pdf', disposition: 'attachment'
-    }
+    respond_to do |format|
+      format.pdf {
+        send_data render_to_string, filename: 'Scout Detailed Report.pdf', type: 'application/pdf', disposition: 'attachment'
+      }
+    end
   end
-end
 
 end
