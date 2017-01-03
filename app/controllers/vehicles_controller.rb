1fa05f79c6a7faff2b00391a0c3b507bbcbdf0b6
bob <rkoertge@salesforce.com>
2017-01-03 00:41:57 -0500

    flag on vehicle to denote ability to tow trailer

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -49,6 +49,6 @@ class VehiclesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def vehicle_params
-    params.require(:vehicle).permit(:name, :belts, :plate)
+    params.require(:vehicle).permit(:name, :belts, :plate, :hitch)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103050539) do
+ActiveRecord::Schema.define(version: 20170103052915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -289,6 +289,7 @@ ActiveRecord::Schema.define(version: 20170103050539) do
     t.string  "name"
     t.integer "belts"
     t.string  "plate"
+    t.boolean "hitch", default: false
   end
 
   create_table "youth_trainings", force: :cascade do |t|
