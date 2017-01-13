2b780fe91e9d83643e01dcf01efb21b5d03a3b78
Bob Koertge <rkoertge@salesforce.com>
2017-01-13 00:44:31 -0500

    save event units

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -94,7 +94,7 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:title, :description, :start, :end, :allDay, :category,
+    params.require(:event).permit(:title, :description, :start, :end, :allDay, :category, :logged_units,
                                   :external_link, :location_id )
   end
 end
