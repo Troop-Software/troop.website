b614f2111d4f7c891750b1cee6989e8a6dd429bd
Bob Koertge <rkoertge@salesforce.com>
2017-01-14 15:32:14 -0500

    removed debugging lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :lockable, :zxcvbnable, :confirmable if Rails.configuration.troop_website.confirm_signup
+         :lockable, :zxcvbnable, :confirmable
 
 
   has_many :articles
